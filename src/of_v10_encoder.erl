17bd90a613be7ccd372f75704d027e1c9bd0db3b
Bruno Rijsman <brunorijsman@hotmail.com>
2011-12-27 16:51:44 -0500

    Progress on v1.0 encoder (not compiling)

--- src/of_v10_encoder.erl
+++ src/of_v10_encoder.erl
@@ -39,66 +39,6 @@ encode_string(String, Length)
     PaddedString = lists:concat([String, ZeroPadding]),
     list_to_binary(PaddedString).
 
--spec encode_header(#of_v10_header{}) -> binary().
-
-encode_header(Header) ->
-    #of_v10_header{version = Version, 
-                   type    = Type, 
-                   length  = Length,
-                   xid     = Xid
-                  } = Header,
-    ?OF_V10_HEADER_PATTERN.
-
--spec encode_body(of_v10_message()) -> {of_v10_message_type(), binary()}.
-
-encode_body(BodyRec) ->
-    if
-        is_record(BodyRec, of_v10_hello)                  -> encode_hello(BodyRec);
-        is_record(BodyRec, of_v10_error)                  -> encode_error(BodyRec);
-        is_record(BodyRec, of_v10_echo_request)           -> encode_echo_request(BodyRec);
-        is_record(BodyRec, of_v10_echo_reply)             -> encode_echo_reply(BodyRec);
-        is_record(BodyRec, of_v10_vendor)                 -> encode_vendor(BodyRec);
-        is_record(BodyRec, of_v10_features_request)       -> encode_features_request(BodyRec);
-        is_record(BodyRec, of_v10_features_reply)         -> encode_features_reply(BodyRec)
-        %% is_record(BodyRec, of_v10_queue_get_config_reply) -> encode_queue_get_config_reply(BodyRec)
-    end.
-
-encode_hello(_HelloRec) ->
-    _FutureExtension = <<>>,
-    {?OF_V10_MESSAGE_TYPE_HELLO, ?OF_V10_HELLO_PATTERN}.
-
-encode_error(ErrorRec) ->
-    #of_v10_error{type = Type, code = Code, data = Data} = ErrorRec,
-    {?OF_V10_MESSAGE_TYPE_ERROR, ?OF_V10_ERROR_PATTERN}.
-
-encode_echo_request(EchoRequestRec) ->
-    #of_v10_echo_request{data = Data} = EchoRequestRec,
-    {?OF_V10_MESSAGE_TYPE_ECHO_REQUEST, ?OF_V10_ECHO_REQUEST_PATTERN}.
-
-encode_echo_reply(EchoReplyRec) ->
-    #of_v10_echo_reply{data = Data} = EchoReplyRec,
-    {?OF_V10_MESSAGE_TYPE_ECHO_REPLY, ?OF_V10_ECHO_REPLY_PATTERN}.
-
-encode_vendor(VendorRec) ->
-    #of_v10_vendor{vendor_id = VendorId, data = Data} = VendorRec,
-    {?OF_V10_MESSAGE_TYPE_VENDOR, ?OF_V10_VENDOR_PATTERN}.
-
-encode_features_request(_FeaturesRequestRec) ->
-    {?OF_V10_MESSAGE_TYPE_FEATURES_REQUEST, ?OF_V10_FEATURES_REQUEST_PATTERN}.
-
-encode_features_reply(FeaturesReplyRec) ->
-    #of_v10_features_reply{data_path_id = DataPathId, 
-                           n_buffers = NBuffers,
-                           n_tables = NTables,
-                           capabilities = CapabilitiesRec,
-                           actions = ActionsRec,
-                           ports = PortsList} = FeaturesReplyRec,
-    Capabilities = encode_capabilities(CapabilitiesRec),
-    Actions = encode_actions_bitmap(ActionsRec),
-    Ports = encode_phy_ports(PortsList),
-    _Pad1 = 0,
-    {?OF_V10_MESSAGE_TYPE_FEATURES_REPLY, ?OF_V10_FEATURES_REPLY_PATTERN}.
-
 encode_capabilities(CapabilitiesRec) ->
     FlowStats  = encode_bool(CapabilitiesRec#of_v10_capabilities.flow_stats),
     TableStats = encode_bool(CapabilitiesRec#of_v10_capabilities.table_stats),
@@ -183,11 +123,170 @@ encode_phy_port_features(FeaturesRec) ->
     _Reserved         = 0,
     ?OF_V10_PHY_PORT_FEATURES_PATTERN.
 
+encode_switch_config_flags(FragHandling) ->
+    _Reserved = 0,
+    ?OF_V10_SWITCH_CONFIG_FLAGS_PATTERN.
+
+encode_switch_config(SwitchConfigRec) ->
+    Flags       = encode_switch_config_flags(SwitchConfigRec#of_v10_switch_config.frag_handling),
+    MissSendLen = SwitchConfigRec#of_v10_switch_config.miss_send_len,
+    ?OF_V10_SWITCH_CONFIG_PATTERN.
+
+encode_flow_match_wildcards(WildCardsRec) ->
+    InPort    = encode_bool(WildCardsRec#of_v10_flow_match_wildcards.in_port),
+    DlVlan    = encode_bool(WildCardsRec#of_v10_flow_match_wildcards.dl_vlan),
+    DlSrc     = encode_bool(WildCardsRec#of_v10_flow_match_wildcards.dl_src),
+    DlDst     = encode_bool(WildCardsRec#of_v10_flow_match_wildcards.dl_dst),
+    DlType    = encode_bool(WildCardsRec#of_v10_flow_match_wildcards.dl_type),
+    NwProto   = encode_bool(WildCardsRec#of_v10_flow_match_wildcards.nw_proto),
+    TpSrc     = encode_bool(WildCardsRec#of_v10_flow_match_wildcards.tp_src),
+    TpDst     = encode_bool(WildCardsRec#of_v10_flow_match_wildcards.tp_dst),
+    NwSrcBits = WildCardsRec#of_v10_flow_match_wildcards.nw_src_bits,
+    NwDstBits = WildCardsRec#of_v10_flow_match_wildcards.nw_dst_bits,
+    DlVlanPcp = encode_bool(WildCardsRec#of_v10_flow_match_wildcards.dl_vlan_pcp),
+    NwTos     = encode_bool(WildCardsRec#of_v10_flow_match_wildcards.nw_tos),
+    _Reserved = 0,
+    ?OF_V10_FLOW_MATCH_WILDCARDS_PATTERN.
+
+encode_flow_match(FlowMatchRec) ->
+    Wildcards = encode_flow_match_wildcards(FlowMatchRec#of_v10_flow_match.wildcards),
+    #of_v10_flow_match{in_port     = InPort,
+                       dl_src      = DlSrc,
+                       dl_dst      = DlDst,
+                       dl_vlan     = DlVlan,
+                       dl_vlan_pcp = DlVlanPcp,
+                       dl_type     = DlType,
+                       nw_tos      = NwTos,
+                       nw_proto    = NwProto,
+                       nw_src      = NwSrc,
+                       nw_dst      = NwDst,
+                       tp_src      = TpSrc,
+                       tp_dst      = TpDst} = FlowMatchRec,
+    _Pad1 = 0,
+    _Pad2 = 0,
+    ?OF_V10_FLOW_MATCH_PATTERN.
+
+encode_actions(ActionList) ->
+    %% CONTINUTE FROM HERE.
+    [].
+    
+
+-spec encode_header(#of_v10_header{}) -> binary().
+encode_header(Header) ->
+    #of_v10_header{version = Version, 
+                   type    = Type, 
+                   length  = Length,
+                   xid     = Xid
+                  } = Header,
+    ?OF_V10_HEADER_PATTERN.
+
+-spec encode_body(of_v10_message()) -> {of_v10_message_type(), binary()}.
+
+encode_body(BodyRec) ->
+    if
+        is_record(BodyRec, of_v10_hello)                  -> encode_hello(BodyRec);
+        is_record(BodyRec, of_v10_error)                  -> encode_error(BodyRec);
+        is_record(BodyRec, of_v10_echo_request)           -> encode_echo_request(BodyRec);
+        is_record(BodyRec, of_v10_echo_reply)             -> encode_echo_reply(BodyRec);
+        is_record(BodyRec, of_v10_vendor)                 -> encode_vendor(BodyRec);
+        is_record(BodyRec, of_v10_features_request)       -> encode_features_request(BodyRec);
+        is_record(BodyRec, of_v10_features_reply)         -> encode_features_reply(BodyRec);
+        is_record(BodyRec, of_v10_get_config_request)     -> encode_get_config_request(BodyRec);
+        is_record(BodyRec, of_v10_get_config_reply)       -> encode_get_config_reply(BodyRec);
+        is_record(BodyRec, of_v10_set_config)             -> encode_set_config(BodyRec);
+        is_record(BodyRec, of_v10_packet_in)              -> encode_packet_in(BodyRec);
+        is_record(BodyRec, of_v10_flow_removed)           -> encode_flow_removed(BodyRec);
+        is_record(BodyRec, of_v10_port_status)            -> encode_port_status(BodyRec);
+        is_record(BodyRec, of_v10_packet_out)             -> encode_packet_out(BodyRec)
+    end.
+
+encode_hello(_HelloRec) ->
+    _FutureExtension = <<>>,
+    {?OF_V10_MESSAGE_TYPE_HELLO, ?OF_V10_HELLO_PATTERN}.
+
+encode_error(ErrorRec) ->
+    #of_v10_error{type = Type, code = Code, data = Data} = ErrorRec,
+    {?OF_V10_MESSAGE_TYPE_ERROR, ?OF_V10_ERROR_PATTERN}.
+
+encode_echo_request(EchoRequestRec) ->
+    #of_v10_echo_request{data = Data} = EchoRequestRec,
+    {?OF_V10_MESSAGE_TYPE_ECHO_REQUEST, ?OF_V10_ECHO_REQUEST_PATTERN}.
+
+encode_echo_reply(EchoReplyRec) ->
+    #of_v10_echo_reply{data = Data} = EchoReplyRec,
+    {?OF_V10_MESSAGE_TYPE_ECHO_REPLY, ?OF_V10_ECHO_REPLY_PATTERN}.
+
+encode_vendor(VendorRec) ->
+    #of_v10_vendor{vendor_id = VendorId, data = Data} = VendorRec,
+    {?OF_V10_MESSAGE_TYPE_VENDOR, ?OF_V10_VENDOR_PATTERN}.
+
+encode_features_request(_FeaturesRequestRec) ->
+    {?OF_V10_MESSAGE_TYPE_FEATURES_REQUEST, ?OF_V10_FEATURES_REQUEST_PATTERN}.
+
+encode_features_reply(FeaturesReplyRec) ->
+    #of_v10_features_reply{data_path_id = DataPathId, 
+                           n_buffers = NBuffers,
+                           n_tables = NTables,
+                           capabilities = CapabilitiesRec,
+                           actions = ActionsRec,
+                           ports = PortsList} = FeaturesReplyRec,
+    Capabilities = encode_capabilities(CapabilitiesRec),
+    Actions = encode_actions_bitmap(ActionsRec),
+    Ports = encode_phy_ports(PortsList),
+    _Pad1 = 0,
+    {?OF_V10_MESSAGE_TYPE_FEATURES_REPLY, ?OF_V10_FEATURES_REPLY_PATTERN}.
+
+encode_get_config_request(_GetConfigRequestRec) ->
+    {?OF_V10_MESSAGE_TYPE_GET_CONFIG_REQUEST, ?OF_V10_GET_CONFIG_REQUEST_PATTERN}.
+
+encode_get_config_reply(GetConfigReplyRec) ->
+    SwitchConfig = encode_switch_config(GetConfigReplyRec#of_v10_get_config_reply.switch_config),
+    {?OF_V10_MESSAGE_TYPE_GET_CONFIG_REPLY, ?OF_V10_GET_CONFIG_REPLY_PATTERN}.
+
+encode_set_config(SetConfigRec) ->
+    SwitchConfig = encode_switch_config(SetConfigRec#of_v10_set_config.switch_config),
+    {?OF_V10_MESSAGE_TYPE_SET_CONFIG, ?OF_V10_SET_CONFIG_PATTERN}.
+
+encode_packet_in(PacketInRec) ->
+    #of_v10_packet_in{buffer_id = BufferId,
+                      total_len = TotalLen,
+                      in_port   = InPort,
+                      reason    = Reason,
+                      data      = Data} = PacketInRec,
+    _Pad = 0,
+    {?OF_V10_MESSAGE_TYPE_PACKET_IN, ?OF_V10_PACKET_IN_PATTERN}.
+
+encode_flow_removed(FlowRemovedRec) ->
+    Match = encode_flow_match(FlowRemovedRec#of_v10_flow_removed.match),
+    #of_v10_flow_removed{cookie        = Cookie,
+                         priority      = Priority,
+                         reason        = Reason,
+                         duration_sec  = DurationSec,
+                         duration_nsec = DurationNsec,
+                         idle_timeout  = IdleTimeout,
+                         packet_count  = PacketCount,
+                         byte_count    = ByteCount} = FlowRemovedRec,
+    _Pad1 = 0,
+    _Pad2 = 0,
+    {?OF_V10_MESSAGE_TYPE_FLOW_REMOVED, ?OF_V10_FLOW_REMOVED_PATTERN}.
+
+encode_port_status(PortStatusRec) ->
+    Desc   = encode_phy_port(PortStatusRec#of_v10_port_status.desc),
+    Reason = PortStatusRec#of_v10_port_status.reason,
+    _Pad   = 0,
+    {?OF_V10_MESSAGE_TYPE_PORT_STATUS, ?OF_V10_PORT_STATUS_PATTERN}.
+
+encode_packet_out(PacketOutRec) ->
+    Actions = encode_actions(PacketOutRec#of_v10_packet_out.actions),
+    #of_v10_packet_out{buffer_id = BufferId,
+                       in_port   = InPort,
+                       actions   = ActionRecs,
+                       data      = Data} = PacketOutRec,
+    {?OF_V10_MESSAGE_TYPE_PACKET_OUT, ?OF_V10_PACKET_OUT_PATTERN}.
+
 %%
 %% Unit tests.
 %%
-
-%% TODO: Test cases for encode string
 
 encode_bool_true_test() ->
     ?assertEqual(1, encode_bool(true)).
@@ -279,4 +378,40 @@ encode_body_features_reply_test() ->
     Rec = of_v10_test_msgs:features_reply_rec(),
     ActualResult = encode_body(Rec),
     ExpectedResult = {?OF_V10_MESSAGE_TYPE_FEATURES_REPLY, of_v10_test_msgs:features_reply_bin()},
+    ?assertEqual(ExpectedResult, ActualResult).
+
+encode_body_get_config_request_test() ->
+    Rec = of_v10_test_msgs:get_config_request_rec(),
+    ActualResult = encode_body(Rec),
+    ExpectedResult = {?OF_V10_MESSAGE_TYPE_GET_CONFIG_REQUEST, of_v10_test_msgs:get_config_request_bin()},
+    ?assertEqual(ExpectedResult, ActualResult).
+
+encode_body_get_config_reply_test() ->
+    Rec = of_v10_test_msgs:get_config_reply_rec(),
+    ActualResult = encode_body(Rec),
+    ExpectedResult = {?OF_V10_MESSAGE_TYPE_GET_CONFIG_REPLY, of_v10_test_msgs:get_config_reply_bin()},
+    ?assertEqual(ExpectedResult, ActualResult).
+
+encode_body_set_config_test() ->
+    Rec = of_v10_test_msgs:set_config_rec(),
+    ActualResult = encode_body(Rec),
+    ExpectedResult = {?OF_V10_MESSAGE_TYPE_SET_CONFIG, of_v10_test_msgs:set_config_bin()},
+    ?assertEqual(ExpectedResult, ActualResult).
+
+encode_body_packet_in_test() ->
+    Rec = of_v10_test_msgs:packet_in_rec(),
+    ActualResult = encode_body(Rec),
+    ExpectedResult = {?OF_V10_MESSAGE_TYPE_PACKET_IN, of_v10_test_msgs:packet_in_bin()},
+    ?assertEqual(ExpectedResult, ActualResult).
+
+encode_body_flow_removed_test() ->
+    Rec = of_v10_test_msgs:flow_removed_rec(),
+    ActualResult = encode_body(Rec),
+    ExpectedResult = {?OF_V10_MESSAGE_TYPE_FLOW_REMOVED, of_v10_test_msgs:flow_removed_bin()},
+    ?assertEqual(ExpectedResult, ActualResult).
+
+encode_body_port_status_test() ->
+    Rec = of_v10_test_msgs:port_status_rec(),
+    ActualResult = encode_body(Rec),
+    ExpectedResult = {?OF_V10_MESSAGE_TYPE_PORT_STATUS, of_v10_test_msgs:port_status_bin()},
     ?assertEqual(ExpectedResult, ActualResult).
